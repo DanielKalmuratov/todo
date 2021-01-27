c6dd3bff538b66beb324a2d8e2525013405f2819
daniel <danielkalmuratov@gmail.com>
2021-01-27 18:00:21 +0600

    add class Todo and makemigrates

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 def homepage(request):
     return render(request, 'index.html')
 
+def test2(request):
+    return render(request, 'test.html')
+
 
