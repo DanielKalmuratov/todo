e5fcdc000507d480000ab866dc521e10127335ac
daniel <danielkalmuratov@gmail.com>
2021-01-27 19:07:30 +0600

    added todo list in test page

--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
+
 
 def homepage(request):
     return render(request, 'index.html')
 
 def test2(request):
-    return render(request, 'test.html')
+    todo_list = ToDo.objects.all()
+    print(todo_list)
+    return render(request, 'test.html', {"todo_list": todo_list})
 
 def modal(request):
     return render(request, 'modal.html')
