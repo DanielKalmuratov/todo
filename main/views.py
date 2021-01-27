d7efa8098b86e93297cef46878b07c6e98c83e37
daniel <danielkalmuratov@gmail.com>
2021-01-27 17:29:03 +0600

    remove extra pages and functions

--- main/views.py
+++ main/views.py
@@ -3,11 +3,4 @@ from django.shortcuts import render, HttpResponse
 def homepage(request):
     return render(request, 'index.html')
 
-def test(request):
-    return render(request, 'test.html')
 
-def second(request):
-    return HttpResponse('Test2 page')
-
-def third(request):
-    return HttpResponse('This is page Test3')
